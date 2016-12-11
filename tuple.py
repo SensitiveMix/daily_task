ce8a6634ce8df2a53b2ef4fb482881369f3479cf
sunnoder <m18205253820@163.com>
2016-12-11 23:31:29 +0800

    update

---
+++ tuple.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+L=['Apple','Google','Microsoft','Java','Python','Ruby','PHP','Adam','Bart','lisa']
+# according to index 
+# print value by tuple 
+print(tuple(L)[0])
+print(tuple(L)[4]) 
+print(tuple(L)[-1])
+print(len(L))
